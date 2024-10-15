dbe44a1cad03da11aea40e056f149b9da315420e
Joy Biswas <joybiswas389@gmail.com>
2024-10-15 20:17:27 +0600

    upload directory added with the files inside of it

--- controllers/user/createCategories.js
+++ controllers/user/createCategories.js
@@ -26,14 +26,55 @@ const categories = [
     { id: 27, name: 'Social & Fun' },
     { id: 17, name: 'Sports' },
     { id: 24, name: 'TV Series & Movies' },
-    { id: 1, name: 'Utilities' }
-]
+    { id: 1, name: 'Utilities' },
+    { id: 29, name: 'Animals' },
+    { id: 30, name: 'Anime' },
+    { id: 31, name: 'Art' },
+    { id: 32, name: 'Beauty' },
+    { id: 33, name: 'Blogs' },
+    { id: 34, name: 'Books' },
+    { id: 35, name: 'Celebrities' },
+    { id: 36, name: 'Computers & Tech' },
+    { id: 37, name: 'Do It Yourself' },
+    { id: 38, name: 'Economics' },
+    { id: 39, name: 'Entertainment' },
+    { id: 40, name: 'Funny' },
+    { id: 41, name: 'Games & Apps' },
+    { id: 42, name: 'Jobs & Marketing' },
+    { id: 43, name: 'Politics' },
+    { id: 44, name: 'Programming' },
+    { id: 45, name: 'Quotes' },
+    { id: 46, name: 'Science' },
+    { id: 47, name: 'Self-development' },
+    { id: 48, name: 'Travel' },
+    { id: 49, name: 'Computer & Tech' },
+    { id: 50, name: 'DIY' },
+    { id: 51, name: 'Health & Fitness' },
+    { id: 52, name: 'Programming & Dev' },
+    { id: 53, name: 'Random Talk' }
+];
 
 
 categories.map(async (category) => {
-    const md = await Category({
-        name: category.name
-    })
-    md.generateSlug();
-    await md.save();
-})
+    // Check if the category already exists (case-insensitive)
+    const existingCategory = await Category.findOne({
+        name: { $regex: new RegExp(`^${category.name}$`, 'i') }
+    });
+
+    if (!existingCategory) {
+        // If category doesn't exist, create and save it
+        const newCategory = new Category({
+            name: category.name
+        });
+
+        // Generate slug for the new category
+        newCategory.generateSlug();
+
+        // Save the new category
+        await newCategory.save();
+        console.log(`Category '${category.name}' created.`);
+    } else {
+        // Skip if category already exists
+        console.log(`Category '${category.name}' already exists, skipping.`);
+    }
+});
